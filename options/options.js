91eca658b0ebfa83f588e6d06b8e4bba44aee482
Crowdbotics <engineering@crowdbotics.com>
2023-01-17 14:49:59 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "flat-dew-dev-57485",
+  url: "https://flat-dew-dev-57485.botics.co",
+  api: "https://flat-dew-dev-57485.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
